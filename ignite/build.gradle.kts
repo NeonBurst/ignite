4c6cf61f7b84f994cb08161e6516d61e91e4d0c7
Nestor <nestorchaoberger@gmail.com>
2024-01-16 12:02:30 +0100

    Added AR Core Dependency

--- ignite/build.gradle.kts
+++ ignite/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.ar:core:1.33.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
