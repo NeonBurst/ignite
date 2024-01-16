16e8fe1ad6a96591ed540ed4bef6670c54824216
Nestor <nestorchaoberger@gmail.com>
2024-01-16 15:15:47 +0100

    Check for AR features is performed after camera permission is given otherwise app wil close rather than crash

--- app/src/main/kotlin/com/thermondo/ignite/MainActivity.kt
+++ app/src/main/kotlin/com/thermondo/ignite/MainActivity.kt
@@ -57,47 +57,66 @@ class MainActivity : ComponentActivity() {
 
 
 
-        var mSession: Session? = null
-        // Ensure that Google Play Services for AR and ARCore device profile data are
-        // installed and up to date.
-        try {
-            if (mSession == null) { //Only for safety
-                when (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
-                    ArCoreApk.InstallStatus.INSTALLED -> {
-                        // Success: Safe to create the AR session.
-                        mSession = Session(this)
-                    }
-                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
-                        // When this method returns `INSTALL_REQUESTED`:
-                        // 1. ARCore pauses this activity.
-                        // 2. ARCore prompts the user to install or update Google Play
-                        //    Services for AR (market://details?id=com.google.ar.core).
-                        // 3. ARCore downloads the latest device profile data.
-                        // 4. ARCore resumes this activity. The next invocation of
-                        //    requestInstall() will either return `INSTALLED` or throw an
-                        //    exception if the installation or update did not succeed.
-                        mUserRequestedInstall = false
-                        return
-                    }
-                }
-            }
-        } catch (e: UnavailableUserDeclinedInstallationException) {
-            // Display an appropriate message to the user and return gracefully.
-            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
-                .show()
-            return
-        } /*catch () { // TODO Handle no camera permission
 
-            return  // mSession remains null, since session creation has failed.
-        }*/
-        val ArSession = ARSession()
-        ArSession.createSession()
 
 
 
     }
 
-
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        results: IntArray
+    ) {
+        val cameraPermissionHelper = CameraPermissionHelper(this)
+        super.onRequestPermissionsResult(requestCode, permissions, results)
+        if (!cameraPermissionHelper.hasCameraPermission()) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                .show()
+            /*if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }*/
+            finish()
+        } else {
+            var mSession: Session? = null
+            // Ensure that Google Play Services for AR and ARCore device profile data are
+            // installed and up to date.
+            try {
+                if (mSession == null) { //Only for safety
+                    when (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                        ArCoreApk.InstallStatus.INSTALLED -> {
+                            // Success: Safe to create the AR session.
+                            mSession = Session(this)
+                        }
+                        ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                            // When this method returns `INSTALL_REQUESTED`:
+                            // 1. ARCore pauses this activity.
+                            // 2. ARCore prompts the user to install or update Google Play
+                            //    Services for AR (market://details?id=com.google.ar.core).
+                            // 3. ARCore downloads the latest device profile data.
+                            // 4. ARCore resumes this activity. The next invocation of
+                            //    requestInstall() will either return `INSTALLED` or throw an
+                            //    exception if the installation or update did not succeed.
+                            mUserRequestedInstall = false
+                            return
+                        }
+                    }
+                }
+            } catch (e: UnavailableUserDeclinedInstallationException) {
+                // Display an appropriate message to the user and return gracefully.
+                Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                    .show()
+                return
+            } catch (e: UnknownError) { // TODO Handle no camera permission
+                Toast.makeText(this, "Camera permission is needed to run this application none was given", Toast.LENGTH_LONG)
+                    .show()
+                return  // mSession remains null, since session creation has failed.
+            }
+            val ArSession = ARSession()
+            ArSession.createSession()
+        }
+    }
 }
 
 @Composable
