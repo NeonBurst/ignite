5c572f06910e3bbf03b748467847b2ae3e0d27fd
Nestor <nestorchaoberger@gmail.com>
2024-01-16 13:27:59 +0100

    Added AR Core Dependency to application and checked for and install AR Core APK

--- app/src/main/kotlin/com/thermondo/ignite/MainActivity.kt
+++ app/src/main/kotlin/com/thermondo/ignite/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.thermondo.ignite
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
 import com.thermondo.ignite.ui.theme.IgniteTheme
 import com.thermondo.ingnite.Demo
 
@@ -32,6 +35,53 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    // requestInstall(Activity, true) will triggers installation of
+    // Google Play Services for AR if necessary.
+    var mUserRequestedInstall = true
+
+    override fun onResume() {
+        super.onResume()
+
+        // Check camera permission.
+
+        var mSession: Session? = null
+        // Ensure that Google Play Services for AR and ARCore device profile data are
+        // installed and up to date.
+        try {
+            if (mSession == null) { //Only for safety
+                when (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    ArCoreApk.InstallStatus.INSTALLED -> {
+                        // Success: Safe to create the AR session.
+                        mSession = Session(this)
+                    }
+                    ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                        // When this method returns `INSTALL_REQUESTED`:
+                        // 1. ARCore pauses this activity.
+                        // 2. ARCore prompts the user to install or update Google Play
+                        //    Services for AR (market://details?id=com.google.ar.core).
+                        // 3. ARCore downloads the latest device profile data.
+                        // 4. ARCore resumes this activity. The next invocation of
+                        //    requestInstall() will either return `INSTALLED` or throw an
+                        //    exception if the installation or update did not succeed.
+                        mUserRequestedInstall = false
+                        return
+                    }
+                }
+            }
+        } catch (e: UnavailableUserDeclinedInstallationException) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                .show()
+            return
+        } /*catch () { // TODO Handle no camera permission
+
+            return  // mSession remains null, since session creation has failed.
+        }*/
+
+    }
+
+
 }
 
 @Composable
